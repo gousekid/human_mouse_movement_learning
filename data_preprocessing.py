0170e28066e3bfff03f81dd391117c23bfb7be84
gousekid <gousekid@gmail.com>
2023-09-10 21:57:54 +0900

    second commit

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -1,9 +1,15 @@
+import numpy as np
 import pandas as pd
-import time
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import LSTM, Dense
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+import os
 
-def preprocess_mouse_data(data_path):
+# 데이터 준비
+def preprocess_mouse_data_with_padding(merged_data):
     # 데이터 로드
-    df = pd.read_csv(data_path)
+    df = merged_data
 
     # 전처리 결과를 저장할 리스트
     processed_data = []
@@ -30,11 +36,71 @@ def preprocess_mouse_data(data_path):
             'time_intervals': time_intervals
         })
 
-    return processed_data
+    # 입력 데이터와 목표 데이터를 저장할 리스트
+    X_list = []
+    Y = []
 
-# 데이터 전처리 실행
-data_path = "./data/mouse_data_20230910_151231.csv"
-processed_data = preprocess_mouse_data(data_path)
+    for session in processed_data:
+        start_x, start_y = session['start_point']
+        path = session['path']
+        
+        # 시작점에서의 x, y 좌표와 경로를 따라가는 동안의 x, y 좌표의 변화량들
+        input_data = [start_x, start_y]
+        for i in range(1, len(path)):
+            dx = path[i][0] - path[i-1][0]
+            dy = path[i][1] - path[i-1][1]
+            input_data.extend([dx, dy])
+        X_list.append(input_data)
+        
+        # 목표 데이터 (끝점의 x, y 좌표)
+        Y.append(session['end_point'])
 
-# 전처리된 데이터의 첫 번째 세션 확인
-print(processed_data[0])
+    # 패딩
+    X_padded = pad_sequences(X_list, padding='post', dtype='float32')
+
+    # numpy 배열로 변환
+    Y = np.array(Y)
+
+    return X_padded, Y
+
+# 데이터 로드 및 전처리
+
+
+# 1. 'data' 폴더 내의 모든 CSV 파일 목록 가져오기
+data_folder = './data'
+csv_files = [f for f in os.listdir(data_folder) if f.endswith('.csv')]
+
+# 2. 각 CSV 파일을 로드하고, 하나의 큰 데이터 프레임으로 합치기
+all_data = []
+for csv_file in csv_files:
+    file_path = os.path.join(data_folder, csv_file)
+    df = pd.read_csv(file_path)
+    all_data.append(df)
+
+merged_data = pd.concat(all_data, ignore_index=True)
+
+
+
+# 데이터 로드 및 전처리
+X, Y = preprocess_mouse_data_with_padding(merged_data)
+
+# 데이터 형태 확인
+X.shape, Y.shape
+# 데이터 형태 조정
+X_padded_reshaped = X.reshape(X.shape[0], X.shape[1], 1)
+
+# 모델 구성
+model = Sequential()
+model.add(LSTM(50, input_shape=(X_padded_reshaped.shape[1], 1)))
+model.add(Dense(2, activation='linear'))  # 2개의 출력 (x, y 좌표)
+
+# 모델 컴파일
+model.compile(optimizer='adam', loss='mean_squared_error')
+
+# 모델 학습
+model.fit(X_padded_reshaped, Y, epochs=100, batch_size=5)
+
+# 예측
+predictions = model.predict(X_padded_reshaped)
+
+model.save('./model/mouseprediction.h5')
---
+++ test.py
@@ -0,0 +1,53 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from tensorflow.keras.models import load_model
+
+# 1. 저장된 모델 불러오기
+model_path = './model/mouseprediction.h5'
+loaded_model = load_model(model_path)
+
+# 2. 특정 시작 위치에서 목표 위치까지의 예측 경로 계산
+def predict_path_corrected(model, start, target, max_length):
+    current_point = start
+    path = [current_point]
+
+    # 예측을 반복하여 경로 생성 (예: 50번 반복)
+    for _ in range(50):
+        input_data = [current_point[0], current_point[1]]
+        
+        # 입력 데이터를 모델이 기대하는 형태로 변환
+        padded_input = np.zeros((1, max_length, 1))
+        for i in range(len(input_data)):
+            padded_input[0, i, 0] = input_data[i]
+        
+        predicted_offset = model.predict(padded_input)
+        
+        # 예측된 변화량을 현재 위치에 추가
+        next_point = [current_point[0] + predicted_offset[0][0], current_point[1] + predicted_offset[0][1]]
+        path.append(next_point)
+        
+        # 목표 위치에 도달하면 중단
+        if np.linalg.norm(np.array(next_point) - np.array(target)) < 5:  # 5는 임계값, 조정 가능
+            break
+
+        current_point = next_point
+
+    return path
+
+start_point = [10, 10]  # 예: [10, 10]
+target_point = [200, 200]  # 예: [200, 200]
+
+input_shape = loaded_model.input_shape[1]
+predicted_path_corrected = predict_path_corrected(loaded_model, start_point, target_point, input_shape)
+
+
+# 도식화
+x_coords, y_coords = zip(*predicted_path_corrected)
+
+plt.figure(figsize=(10, 10))
+plt.plot(x_coords, y_coords, '-o', label='Predicted Path')
+plt.scatter(*target_point, color='red', label='Target Point')
+plt.legend()
+plt.grid(True)
+plt.title('Predicted Mouse Movement Path')
+plt.show()
